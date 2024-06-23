3f03e4c77abb02d244a84c64e2885f727791202b
unknown <abdallahzyada25@gmail.com>
2024-06-24 00:49:43 +0300

    Updating product quantities in the cart

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -10,7 +10,7 @@ from .forms import CartAddProductForm
 def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
-    form = CartAddProductForm(request.post)
+    form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(
@@ -30,6 +30,11 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial=
+                                                          {'quantity': item['quantity'],
+                                                           'override': True})
+
     return render(request,
                   'cart/detail.html',
                   {'cart':cart})
